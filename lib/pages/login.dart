1abca918254902d3ce3e5888ac91eca6db58f630
kasem <youremail@yourdomain.com>
2023-05-24 14:10:47 +0300

    add sized box bettwen tow text field

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,35 +1,41 @@
 import 'package:flutter/material.dart';
 
-
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-  body:Center(
-    child: Padding(
-      padding: const EdgeInsets.all(33.0),
-      child: Column(
-        children: [
-      const SizedBox(height:64),
-      TextField(
-        keyboardType: TextInputType.text,
-        obscureText: true,
-        decoration: InputDecoration(
-          hintText: "Enter Your Password : ",
-          // To delete borders
-          enabledBorder: OutlineInputBorder(borderSide: Divider.createBorderSide(context),),
-          focusedBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.grey,),),
-          // fillColor: Colors.red,
-          filled: true,
-          contentPadding: const EdgeInsets.all(8),
-        ))
-      
-      
-      ]),
-    ),
-  ) ,
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(33.0),
+          child: Column(children: [
+            const SizedBox(height: 64),
+            TextField(
+                keyboardType: TextInputType.text,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: "Enter Your Password : ",
+                  // To delete borders
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: Divider.createBorderSide(context),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                  ),
+                  // fillColor: Colors.red,
+                  filled: true,
+                  contentPadding: const EdgeInsets.all(8),
+                )),
+
+                SizedBox(
+                  height: 16,
+                )
+          ]),
+        ),
+      ),
     );
   }
 }
