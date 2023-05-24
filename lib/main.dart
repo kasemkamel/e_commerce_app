485e2f8afc2174fa54f6b1e0e7253f6d973a77b0
kasem <youremail@yourdomain.com>
2023-05-24 13:58:04 +0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -10,5 +10,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return Scaffold(
+    
+  
+
      );}}
