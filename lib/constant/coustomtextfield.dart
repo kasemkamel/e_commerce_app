d44e897238515b30d66dda977e364eb959bfadb8
kasem <youremail@yourdomain.com>
2023-05-24 14:15:29 +0300

    class Mytextfeild

---
+++ lib/constant/coustomtextfield.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+class Mytextfeild extends StatelessWidget {
+  const Mytextfeild({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
